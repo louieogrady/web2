bf2a5fde41fee3247016732b824414d43da6ecf3
louieogrady <l.ogrady@gmail.com>
2021-10-06 17:50:03 +0100

    sorted basic media queries

--- src/components/panel/panel.component.tsx
+++ src/components/panel/panel.component.tsx
@@ -9,8 +9,8 @@ interface PanelProps {
 
 const Panel: React.FC<PanelProps> = ({ title, text, theme = "white" }): JSX.Element => {
   // Regexs for helping parse text props 
-  const mailtoRegex = new RegExp('mailto:([^\?]*)');
-  const linkRegex = new RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})');
+  const mailtoRegex = new RegExp('mailto:([^?]*)');
+  const linkRegex = new RegExp('(https?:(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})');
 
   /** Crude parser for rendering correct html content for text content props */
   const parseLine = (line: string) => {
@@ -21,7 +21,7 @@ const Panel: React.FC<PanelProps> = ({ title, text, theme = "white" }): JSX.Elem
       return <a href={line}>{emailText} at {emailSplit[1]}</a>;
     } else if (linkRegex.test(line)) {
       const webLinkText = line.split('//')[1];
-      return <a href={line} target="_blank">{webLinkText}</a>;
+      return <a href={line} rel="noreferrer" target="_blank">{webLinkText}</a>;
     } else {
       return <p>{line}</p>;
     }
