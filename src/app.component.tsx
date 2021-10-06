72fc98fd003b0e2cd742b21839d3bb633b3863f2
louieogrady <l.ogrady@gmail.com>
2021-10-06 13:21:51 +0100

    Basic layout on splash page

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -6,14 +6,23 @@ import Panel from './components/panel/panel.component';
 
 function App() {
 
-  const aboutText = ['Hello this is a test text', 'it should split on every comma', 'hopefully it does']
+  const aboutText = [`Louie is a frontend web developer with over 2 year's professional experience.`, ' \n His main experience is with Javascript/Typescript and Angular 2+, but also has experience working with React and Redux Toolkit.', '\n As a web developer he is excited about creating innovative browser based user experiences. But he is also very interested in functional programming and devOps.'];
+  const previousText = ['Louie studied Contemporary Music Composition at the Royal Welsh College of Music (RWCMD) and has an MA in Art from the Manchester School of Art (MIRIAD).', '\n As a multidisciplinary artist, he has over 5 years experience of delivering commissioned work from the initial proposal stage to installing work onsite across Europe.'];
 
   return (
     <div className="App">
       <Header/>
       <Footer/>
-      <div className="PanelContainer">
-        <Panel title="About" text={aboutText} />
+      <div className="PanelColumn">
+        <div className="PanelWrapper">
+          <Panel title="ABOUT" text={aboutText} />
+        </div>
+        <div className="PanelWrapper black">
+          <Panel title="PREVIOUS" text={previousText} theme="black"/>
+        </div>
+        <div className="PanelWrapper">
+          <Panel title="PREVIOUS" text={aboutText} />
+        </div>
       </div>
     </div>
   );
--- src/components/panel/panel.component.tsx
+++ src/components/panel/panel.component.tsx
@@ -4,14 +4,15 @@ import './panel.scss';
 interface PanelProps {
   title: string;
   text: string[];
+  theme?: string
 }
 
-const Panel: React.FC<PanelProps> = ({ title, text }): JSX.Element => {
+const Panel: React.FC<PanelProps> = ({ title, text, theme = "white" }): JSX.Element => {
 
   // lines = 
 
   return (
-    <div className="Panel">
+    <div className={`Panel ${ theme }`}>
       <p className="Title"> { title } </p>
       <div className="TextContainer">
         {text.map((line) => <p>{line}</p>)}
