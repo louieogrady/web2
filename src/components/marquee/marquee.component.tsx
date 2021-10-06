ed825c49a067b1ae13daa7b161b1c2d9aff5a629
louieogrady <l.ogrady@gmail.com>
2021-10-06 16:34:51 +0100

    Committing work prior to media query work

---
+++ src/components/marquee/marquee.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './marquee.scss';
+
+interface MarqueeProps {
+  text: string[];
+}
+
+const Marquee: React.FC<MarqueeProps> = ({ text }): JSX.Element => {
+
+  return (
+    <div> </div>
+  ); 
+}
--- src/components/panel/panel.component.tsx
+++ src/components/panel/panel.component.tsx
@@ -8,14 +8,31 @@ interface PanelProps {
 }
 
 const Panel: React.FC<PanelProps> = ({ title, text, theme = "white" }): JSX.Element => {
+  // Regexs for helping parse text props 
+  const mailtoRegex = new RegExp('mailto:([^\?]*)');
+  const linkRegex = new RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})');
 
-  // lines = 
+  /** Crude parser for rendering correct html content for text content props */
+  const parseLine = (line: string) => {
+
+    if (mailtoRegex.test(line)) {
+      const emailSplit = line.split('@');
+      const emailText = emailSplit[0].replace(/mailto:/, "");
+      return <a href={line}>{emailText} at {emailSplit[1]}</a>;
+    } else if (linkRegex.test(line)) {
+      const webLinkText = line.split('//')[1];
+      return <a href={line} target="_blank">{webLinkText}</a>;
+    } else {
+      return <p>{line}</p>;
+    }
+
+  }
 
   return (
     <div className={`Panel ${ theme }`}>
       <p className="Title"> { title } </p>
       <div className="TextContainer">
-        {text.map((line) => <p>{line}</p>)}
+        {text.map((line) => <p>{parseLine(line)}</p>)}
       </div>
     </div>
   );
