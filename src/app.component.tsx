a8bfbe043868f8cbe3aad37320e6b5af031cd148
louieogrady <l.ogrady@gmail.com>
2022-04-12 13:11:43 +0100

    marquee change and animation changes

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -7,11 +7,11 @@ import Marquee from './components/marquee/marquee.component';
 
 function App() {
 
-  const aboutText = [`Louie is a frontend web developer with over 2 years professional experience.`, '\n \n As a web developer he is excited about creating innovative browser based user experiences. He is also very interested in how blockchain can change the future of the web, functional programming and devOps.', '\n \n He currently works at thortful'];
+  const aboutText = [`Louie is a frontend web developer with over 2 years professional experience.`, '\n \n As a web developer he is excited about creating innovative browser based user experiences. He is also very interested in how blockchain can change the future of the web, functional programming and DevOps.', '\n \n He currently works at thortful'];
   const skillsText = ['TypeScript, JavaScript (ES5/ES6+), Angular 2+, RxJS, React, Redux Toolkit, Jasmine, React Testing Library, Tone.js, Material UI, HTML5, Responsive CSS, REST, Git, Jenkins, npm'];
   const previousText = ['Louie studied Contemporary Music Composition at the Royal Welsh College of Music (RWCMD) and has an MA in Art from the Manchester School of Art (MIRIAD).', '\n \n As a multidisciplinary artist, he has over 5 years experience of delivering commissioned work from the initial proposal stage to installing work onsite across Europe.', '\n \n He has exhibited work at the Wellcome Collection, Brass International Festival, Gallery Miroslav Kraljevic and London Short Film Festival.'];
   const contactText = ['mailto:l.ogrady@gmail.com', 'https://linkedin.com/in/louieogrady', 'https://soundcloud.com/muesliboys'];
-  const marqueeText = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut erat vel quam consectetur convallis in in est. Sed ac nibh accumsan, interdum lorem a, feugiat nunc. Nunc eu mollis est. Quisque at tortor nec metus pretium hendrerit at eu leo. Fusce volutpat euismod sem non commodo. Aenean quis enim.'];
+  const marqueeText = ['oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'];
   const projectText = ['https://rhythm-composer.herokuapp.com'];
 
   // Regexs for parsing text content
@@ -42,7 +42,7 @@ function App() {
       <Marquee text={marqueeText} />
       <div className="PanelColumn">
         <div className="PanelWrapper">
-          <Panel title="ABOUT" >
+          <Panel title="ABOUT" >  
             <p>{aboutText}</p>
           </Panel>
         </div>
